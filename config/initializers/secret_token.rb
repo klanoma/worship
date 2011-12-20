4f281ecf3a2098972f00ad91d99b0de0097c6fd5
William Youmans <will@williamyoumans.com>
2011-12-19 21:54:16 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Worship::Application.config.secret_token = '6221e33df1c98b1e94dc75c83e8887406a07c7819e381c3ff9732fea6e02e351ad571df517b2df843d5502d94b6f82ec599040e35a7f498000aac10a0651decf'
