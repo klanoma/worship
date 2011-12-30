9aa12c916f0af1aed44f2994bcd17134633f84b3
William Youmans <will@williamyoumans.com>
2011-12-30 00:24:57 -0700

    error styles

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,4 +1,12 @@
 class Volunteer < ActiveRecord::Base
+  #relations
   belongs_to:user
   belongs_to:state
+
+  #validation
+  validates :first_name, :last_name, presence: true
+  validates :email, allow_blank: true, format: {
+    with: %r{/\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/}i,
+    message: 'must be a valid'
+  }
 end
