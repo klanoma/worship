eca59a70e003f91f32b87feaf632db2af1a588d9
William Youmans <will@williamyoumans.com>
2011-12-31 03:14:54 -0700

    moved page title bar to layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def page_title
+    (@content_for_title if @content_for_title).to_s
+  end
+
+  def meta_keywords
+    if @content_for_meta_keywords
+      keywords = @content_for_meta_keywords.to_s
+    else
+      keywords = 'worship database, worship log, service log, church archive, worship archive, liturgy, litergical, paster\'s notebook, preacher\'s archive, church administrator archive, search sermon, find sermons'
+    end
+  end
+
+  def meta_description
+    if @content_for_meta_description
+      description = @content_for_meta_description.to_s
+    else
+      description = 'Worship Log provides a searchable'
+    end
+  end
 end
