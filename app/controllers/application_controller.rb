b1d3f0d07718e139e9c51771c1d72d7c3031b302
William Youmans <will@williamyoumans.com>
2012-01-07 16:29:09 -0700

    update devise redirects

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,11 @@ class ApplicationController < ActionController::Base
 
   private
 
-  # Overwriting the sign_out redirect path method
+  #update devise redirects
   def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
+  def after_sign_up_path_for(resource_or_scope)
     new_user_session_path
   end
 end
