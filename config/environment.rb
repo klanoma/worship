4cd2d50c8858440b1da68ebdd6ddaa28001f0e9f
William Youmans <will@williamyoumans.com>
2012-01-07 12:30:47 -0700

    login style update

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,6 @@ Worship::Application.initialize!
 #Action Mailer!
 ActionMailer::Base.perform_deliveries = true
 ActionMailer::Base.raise_delivery_errors = true
-ActionMailer::Base.default_charset = "utf-8"
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   :address => "smtp.gmail.com",
