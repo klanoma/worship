82118d0900700e91778b352e22babe9de6b5818f
William Youmans <will@williamyoumans.com>
2012-01-07 11:47:15 -0700

    action mailer settings

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Worship::Application.initialize!
+
+#Action Mailer!
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.default_charset = "utf-8"
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :address => "smtp.gmail.com",
+  :port => 465,
+  :authentication => :login,
+  :user_name => "webmaster@worshiplog.com",
+  :password => "mollygirl"
+}
