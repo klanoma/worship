da9e55549ce16b3edd003b3dd0afeaa437bfc1fe
William Youmans <will@williamyoumans.com>
2012-01-07 21:25:31 -0700

    js cleanup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -129,10 +129,23 @@ $(function() {
 
   //===== Dynamic data table =====//
 
-  oTable = $('table#volunteers').dataTable({
+  $('table#volunteers').dataTable({
     "bJQueryUI": true,
     "sPaginationType": "full_numbers",
-    "sDom": '<""f>t<"F"lp>',
+    "sDom": '<f>t<"F"lp>',
+    "aoColumns": [
+      null,
+      null,
+      null,
+      {"bSortable" : false, "bSearchable": false},
+      {"bSortable" : false, "bSearchable": false}
+    ]
+  });
+  
+  $('table#services').dataTable({
+    "bJQueryUI": true,
+    "sPaginationType": "full_numbers",
+    "sDom": '<f>t<"F"lp>',
     "aoColumns": [
       null,
       null,
